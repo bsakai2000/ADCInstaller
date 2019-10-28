1ff1f239b5b9a779f39470b28ad751643740cb16
bsakai2000 <brandon.sakai@gmail.com>
2019-10-28 18:23:56 -0400

    Added patch to enable htaccess

--- installer.sh
+++ installer.sh
@@ -6,6 +6,7 @@ sudo -u www-data openssl genrsa -out /var/www/keys/userPrivate.key 4096
 sudo -u www-data openssl rsa -in /var/www/keys/adminPrivate.key -outform PEM -pubout -out adminPublic.key
 sudo -u www-data openssl rsa -in /var/www/keys/userPrivate.key -outform PEM -pubout -out userPublic.key
 sudo a2enmod rewrite
+sudo patch /etc/apache2.conf ./apache2.conf.patch
 #DO_PATCH
 
 echo admin1/admin1pass
