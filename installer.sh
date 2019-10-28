c1457f48b9c7317be0e260144e2e864405deca3b
bsakai2000 <brandon.sakai@gmail.com>
2019-10-28 18:59:53 -0400

    Bugfixes

--- installer.sh
+++ installer.sh
@@ -3,26 +3,23 @@ set -exo pipefail
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt install curl
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install apache2 mysql-server mysql php php-mysql php-mbstring composer make g++ nodejs
+sudo apt install -y apache2 mysql-server php php-mysql php-mbstring composer make g++ nodejs
 sudo mkdir /var/www/keys
 sudo chmod 777 /var/www/keys
 sudo -u www-data openssl genrsa -out /var/www/keys/adminPrivate.key 4096
 sudo -u www-data openssl genrsa -out /var/www/keys/userPrivate.key 4096
-sudo -u www-data openssl rsa -in /var/www/keys/adminPrivate.key -outform PEM -pubout -out adminPublic.key
-sudo -u www-data openssl rsa -in /var/www/keys/userPrivate.key -outform PEM -pubout -out userPublic.key
+sudo -u www-data openssl rsa -in /var/www/keys/adminPrivate.key -outform PEM -pubout -out /var/www/keys/adminPublic.key
+sudo -u www-data openssl rsa -in /var/www/keys/userPrivate.key -outform PEM -pubout -out /var/www/keys/userPublic.key
 sudo a2enmod rewrite
-sudo patch /etc/apache2.conf ./apache2.conf.patch
+sudo patch /etc/apache2/apache2.conf ./apache2.conf.patch
+git clone https://github.com/AutomaticDoorControl/AutoDoorCtrlWebAPIPHP.git
+cd AutoDoorCtrlWebAPIPHP/api
+composer install
+cd ../..
 git clone https://github.com/AutomaticDoorControl/AutoDoorCtrlWeb.git
 cd AutoDoorCtrlWeb
 npm i
-cd ..
-git clone https://github.com/AutomaticDoorControl/AutoDoorCtrlWebAPIPHP.git
-cd AutoDoorCtrlWebAPIPHP
-cd AutoDoorCtrlWebAPI/api
-composer install
-cd ../..
 sudo npm install -g @angular/cli
-cd AutoDoorCtrlWeb
 ng build
 cp dist/*/* /var/www/html
 cd ..
